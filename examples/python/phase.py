9d3622981514ce1b63dbe130a4f881ac45c514bb
Jack Vreeken <jack@vreeken.me>
2023-05-03 14:17:09 +0200

    Style: Fix formatting of examples
    And include the example Python files in the CI pipeline checks as well.

--- examples/python/phase.py
+++ examples/python/phase.py
@@ -28,7 +28,7 @@ z = pyzsf.ZSFUnsteady(15.0, 0.0, **lock_parameters, **boundary_conditions, **ope
 print("State after initialization")
 pprint.pprint(z.state)
 
-print("\nPhase 1:\n" + "*"*8)
+print("\nPhase 1:\n" + "*" * 8)
 
 results = z.step_phase_1(300.0)
 print("Transports:")
@@ -36,21 +36,21 @@ pprint.pprint(results)
 print("State:")
 pprint.pprint(z.state)
 
-print("\nPhase 2:\n" + "*"*8)
+print("\nPhase 2:\n" + "*" * 8)
 results = z.step_phase_2(840.0)
 print("Transports:")
 pprint.pprint(results)
 print("State:")
 pprint.pprint(z.state)
 
-print("\nPhase 3:\n" + "*"*8)
+print("\nPhase 3:\n" + "*" * 8)
 results = z.step_phase_3(300.0)
 print("Transports:")
 pprint.pprint(results)
 print("State:")
 pprint.pprint(z.state)
 
-print("\nPhase 4:\n" + "*"*8)
+print("\nPhase 4:\n" + "*" * 8)
 results = z.step_phase_4(840.0, ship_volume_sea_to_lake=800.0)
 print("Transports:")
 pprint.pprint(results)
