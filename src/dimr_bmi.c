7b30777ec859ab06c353cb6affe4623568dbdc00
Erwin Mulder <160875449+erwin-mulder@users.noreply.github.com>
2024-03-05 12:50:48 +0100

    Added print statements for demo.

--- src/dimr_bmi.c
+++ src/dimr_bmi.c
@@ -18,6 +18,8 @@ int initialize(const char *config_file)
   zsf_param_default(&_parameters);
   // TODO: Implement reading supplied config file.
 
+  printf("ZSF: %s( \"%s\" ) called.\n", __func__, config_file);
+
   status = zsf_initialize_state(&_parameters, &_state, sal_lock, head_lock);
 
   return ZSF_TO_DIMR_STATUS(status);
@@ -26,6 +28,7 @@ int initialize(const char *config_file)
 int finalize()
 {
   // TODO: Implement me
+  printf("ZSF: %s() called.\n", __func__);
   return DIMR_BMI_OK; // Should always return DIMR_BMI_OK
 }
 
@@ -33,6 +36,7 @@ int finalize()
 int set_var(const char *key, void *src_ptr)
 {
   // TODO: Implement me
+  printf("ZSF: %s( \"%s\", %p ) called.\n", __func__, key, src_ptr);
   return DIMR_BMI_OK;
 }
 
@@ -40,6 +44,7 @@ int set_var(const char *key, void *src_ptr)
 int get_var(const char *key, void *dst_ptr)
 {
   // TODO: Implement me
+  printf("ZSF: %s( \"%s\", %p ) called.\n", __func__, key, dst_ptr);
   if (dst_ptr != NULL) {
     //*(double *)dst_ptr = 5.0;
   }
@@ -49,18 +54,21 @@ int get_var(const char *key, void *dst_ptr)
 // In DIMR **dst_ptr always is a double.
 int get_value_ptr(char *key, void **dst_ptr) 
 { 
-  return DIMR_BMI_OK; 
+  printf("ZSF: %s( \"%s\", %p ) called.\n", __func__, key, dst_ptr);
+  return DIMR_BMI_OK;
 }
 
 int update(double dt)
 { 
   int status = zsf_calc_steady(&_parameters, &_results, NULL);
+  printf("ZSF: %s( %g ) called.\n", __func__, dt);
   return ZSF_TO_DIMR_STATUS(status);
 }
 
 int get_var_shape(char *key, int *dims) // dims -> int[6]
 {
   // TODO: Implement me
+  printf("ZSF: %s( \"%s\", %d ) called.\n", __func__, key, *dims);
   return DIMR_BMI_OK;
 }
 
@@ -85,21 +93,25 @@ void get_attribute(char *name, char *value)
 void get_start_time(double *start_time_ptr)
 {
   // TODO: Implement me
+  printf("ZSF: %s( %g ) called.\n", __func__, *start_time_ptr);
 }
 
 void get_end_time(double *end_time_ptr)
 {
   // TODO: Implement me
+  printf("ZSF: %s( %g ) called.\n", __func__, *end_time_ptr);
 }
 
 void get_time_step(double *time_step_ptr)
 {
   // TODO: Implement me
+  printf("ZSF: %s( %g ) called.\n", __func__, *time_step_ptr);
 }
 
 void get_current_time(double *current_time_ptr)
 {
   // TODO: Implement me
+  printf("ZSF: %s( %g ) called.\n", __func__, *current_time_ptr);
 }
 
 // Points to a Log object (see log.h in DIMR)
