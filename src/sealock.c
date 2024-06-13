e3ad5089db646358c07b03abf2aaa91de8751204
Erwin Mulder <160875449+erwin-mulder@users.noreply.github.com>
2024-06-13 12:02:19 +0200

    Renamed some functions to better describe what they do.

--- src/sealock.c
+++ src/sealock.c
@@ -4,10 +4,10 @@
 #include "load_time_averaged.h"
 #include "sealock.h"
 
-int sealock_load_data(sealock_state_t *lock, char *filepath) {
+int sealock_load_timeseries(sealock_state_t *lock, char *filepath) {
   int status = SEALOCK_OK;
   // read csv data
-  if (!load_time_averaged_data(&lock->timeseries_data, filepath)) {
+  if (!load_time_averaged_timeseries(&lock->timeseries_data, filepath)) {
     lock->current_row = 0;
     lock->times_len = get_csv_num_rows(&lock->timeseries_data);
     if (lock->times_len == 0) {
@@ -32,28 +32,28 @@ void _sealock_update_current_row(sealock_state_t *lock, double time) {
   lock->current_row = row ? row - 1 : 0;
 }
 
-int _sealock_update_cycle_average_data(sealock_state_t *lock, double time) {
+int _sealock_update_cycle_average_parameters(sealock_state_t *lock, double time) {
   _sealock_update_current_row(lock, time);
   return get_csv_row_data(&lock->timeseries_data, lock->current_row, &lock->parameters) == CSV_OK
              ? SEALOCK_OK
              : SEALOCK_ERROR;
 }
 
-int _sealock_update_phase_wise_data(sealock_state_t *lock, double time) {
+int _sealock_update_phase_wise_parameters(sealock_state_t *lock, double time) {
   _sealock_update_current_row(lock, time);
   // TODO: implement me: See UNST-7866.
   return SEALOCK_OK;
 }
 
-int sealock_update_data(sealock_state_t *lock, double time) {
+int sealock_set_parameters_for_time(sealock_state_t *lock, double time) {
   int status = SEALOCK_OK;
 
   switch (lock->computation_mode) {
   case cycle_average_mode:
-    status = _sealock_update_cycle_average_data(lock, time);
+    status = _sealock_update_cycle_average_parameters(lock, time);
     break;
   case phase_wise_mode:
-    status = _sealock_update_phase_wise_data(lock, time);
+    status = _sealock_update_phase_wise_parameters(lock, time);
     break;
   default:
     status = SEALOCK_ERROR; // Should never happen.
@@ -63,7 +63,7 @@ int sealock_update_data(sealock_state_t *lock, double time) {
 }
 
 int sealock_update(sealock_state_t *lock, double time) {
-  int status = sealock_update_data(lock, time);
+  int status = sealock_set_parameters_for_time(lock, time);
   if (status == SEALOCK_OK) {
     switch (lock->computation_mode) {
     case cycle_average_mode:
