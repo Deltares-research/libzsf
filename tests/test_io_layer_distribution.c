06d7b39d19676d1a3266db1d092383bd0f3f0398
Harmen Wierenga <harmen.wierenga@gmail.com>
2024-07-02 10:19:19 +0200

    Add method to integrate piecewise linear profile, use it to distribute discharge over layers proportional to cell volumes

---
+++ tests/test_io_layer_distribution.c
@@ -0,0 +1,128 @@
+#include "io_layer_distribution.h"
+#include "unity.h"
+
+static const double epsilon = 1.0e-8;
+
+void setUp(void) {}
+
+void tearDown(void) {}
+
+static int create_linear_profile(profile *profile, const int number_of_positions,
+                                 const double discharge_0, const double discharge_1) {
+  profile->number_of_positions = number_of_positions;
+  profile->relative_z_position = malloc(number_of_positions * sizeof(double));
+  profile->relative_discharge_from_lock = malloc(number_of_positions * sizeof(double));
+
+  if (profile->relative_z_position == NULL || profile->relative_discharge_from_lock == NULL) {
+    return -1;
+  }
+
+  const double slope = (discharge_1 - discharge_0) / 1.0;
+  const double z_step = 1.0 / (number_of_positions - 1);
+
+  for (int i = 0; i < number_of_positions; ++i) {
+    const double z = z_step * i;
+    const double d = discharge_0 + slope * z;
+    profile->relative_z_position[i] = z;
+    profile->relative_discharge_from_lock[i] = d;
+  }
+  return 0;
+}
+
+static double expected_integration_result(const double lower_bound, const double upper_bound,
+                                          const double discharge_0, const double discharge_1) {
+  const double slope = (discharge_1 - discharge_0) / 1.0;
+  return discharge_0 * (upper_bound - lower_bound) +
+         0.5 * slope * (pow(upper_bound, 2.0) - pow(lower_bound, 2.0));
+}
+
+static void test_integrate_constant_profile(void) {
+  const double constant_value = 1.0;
+  const double lower_bound = 0.3;
+  const double upper_bound = 0.7;
+  profile constant_profile = profile_default;
+  TEST_ASSERT_EQUAL(create_linear_profile(&constant_profile, 2, constant_value, constant_value), 0);
+
+  const double result =
+      integrate_piecewise_linear_profile(&constant_profile, lower_bound, upper_bound);
+  TEST_ASSERT_DOUBLE_WITHIN(
+      epsilon,
+      expected_integration_result(lower_bound, upper_bound, constant_value, constant_value),
+      result);
+  cleanup_profile(&constant_profile);
+}
+
+static void test_integrate_to_edges(void) {
+  const double constant_value = 0.618;
+  const double lower_bound = 0.0;
+  const double upper_bound = 1.0;
+  profile constant_profile = profile_default;
+  TEST_ASSERT_EQUAL(create_linear_profile(&constant_profile, 2, constant_value, constant_value), 0);
+
+  const double result =
+      integrate_piecewise_linear_profile(&constant_profile, lower_bound, upper_bound);
+  TEST_ASSERT_DOUBLE_WITHIN(
+      epsilon,
+      expected_integration_result(lower_bound, upper_bound, constant_value, constant_value),
+      result);
+  cleanup_profile(&constant_profile);
+}
+
+static void test_integrate_linear_profile(void) {
+  const double lower_bound = 0.273;
+  const double upper_bound = 0.81;
+  const double left_value = 0.1;
+  const double right_value = 4.33;
+  profile linear_profile = profile_default;
+  TEST_ASSERT_EQUAL(create_linear_profile(&linear_profile, 6, left_value, right_value), 0);
+
+  const double result =
+      integrate_piecewise_linear_profile(&linear_profile, lower_bound, upper_bound);
+  TEST_ASSERT_DOUBLE_WITHIN(
+      epsilon, expected_integration_result(lower_bound, upper_bound, left_value, right_value),
+      result);
+  cleanup_profile(&linear_profile);
+}
+
+static void test_distribute_over_two_layers(void) {
+  // Create a normalized profile, so that calculating the expected outcome is simpler
+  const double left_profile_value = 0.0;
+  const double right_profile_value = 2.0;
+  profile linear_profile = profile_default;
+  TEST_ASSERT_EQUAL(
+      create_linear_profile(&linear_profile, 10, left_profile_value, right_profile_value), 0);
+
+  layers two_layers = layers_default;
+  two_layers.number_of_layers = 2;
+  two_layers.normalized_target_volumes = malloc(sizeof(double) * two_layers.number_of_layers);
+  TEST_ASSERT_NOT_EQUAL(two_layers.normalized_target_volumes, NULL);
+  two_layers.normalized_target_volumes[0] = 0.3;
+  two_layers.normalized_target_volumes[1] = 0.7;
+
+  const double total_discharge = 95.13;
+  layered_discharge result = layered_discharge_default;
+  TEST_ASSERT_EQUAL(
+      distribute_discharge_over_layers(total_discharge, &linear_profile, &two_layers, &result), 0);
+
+  TEST_ASSERT_EQUAL(result.number_of_layers, 2);
+  const double expected_first_discharge =
+      total_discharge * pow(two_layers.normalized_target_volumes[0], 2.0);
+  const double expected_second_discharge = total_discharge - expected_first_discharge;
+  TEST_ASSERT_DOUBLE_WITHIN(epsilon, expected_first_discharge, result.discharge_per_layer[0]);
+  TEST_ASSERT_DOUBLE_WITHIN(epsilon, expected_second_discharge, result.discharge_per_layer[1]);
+
+  cleanup_profile(&linear_profile);
+  cleanup_layers(&two_layers);
+  cleanup_layered_discharge(&result);
+}
+
+int main(void) {
+  UNITY_BEGIN();
+
+  RUN_TEST(test_integrate_constant_profile);
+  RUN_TEST(test_integrate_to_edges);
+  RUN_TEST(test_integrate_linear_profile);
+  RUN_TEST(test_distribute_over_two_layers);
+
+  return UNITY_END();
+}
