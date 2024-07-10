efe3009c136f9c99dc46406b10b59fa3f470ecae
Erwin Mulder <160875449+erwin-mulder@users.noreply.github.com>
2024-07-10 17:00:00 +0200

    Added skeleton unittest for load_csv.

---
+++ tests/test_load_csv.c
@@ -0,0 +1,17 @@
+#include "csv/load_csv.h"
+#include "unity.h"
+#include <string.h>
+
+void setUp(void) {}
+
+void tearDown(void) {}
+
+static void test_read_csv() {}
+
+int main(void) {
+  UNITY_BEGIN();
+
+  RUN_TEST(test_read_csv);
+
+  return UNITY_END();
+}
