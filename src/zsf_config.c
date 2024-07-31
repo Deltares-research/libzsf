45c3395ae23667011e56842e370af81066091e15
Erwin Mulder <160875449+erwin-mulder@users.noreply.github.com>
2024-07-31 17:13:20 +0200

    Added reading of flow_profile setting from .ini file.

--- src/zsf_config.c
+++ src/zsf_config.c
@@ -9,7 +9,40 @@
 #include <stdlib.h>
 #include <string.h>
 
-int _zsf_ini_handler(char *section, char *key, char *value, void *data_ptr) {
+// Parse a comma separated list of doubles in value string.
+// Returns number of elements found in the list or 0 on error.
+// If array_ptr is not NULL, the array will be filled with converted double values.
+// NOTE: The caller is responsible for deallocating the array.
+static int parse_double_list(char* value, double** array_ptr) {
+  int num_items = 0;
+  double *array = NULL;
+  char *tempstr = strdup(value);
+  char *token;
+
+  token = strtok(tempstr, ",");
+  while (token) {
+    num_items++;
+    token = strtok(NULL, ",");
+  }
+  free(tempstr);
+
+  if (num_items && array_ptr != NULL) {
+    array = malloc(num_items * sizeof(double));
+    if (array) {
+      token = strtok(value, ",");
+      for (int i = 0; i < num_items && token; i++) {
+        array[i] = strtod(token, NULL);
+        token = strtok(NULL, ",");
+      }
+    }
+    *array_ptr = array;
+  }
+
+  return num_items;
+}
+
+
+static int zsf_ini_handler(char *section, char *key, char *value, void *data_ptr) {
   zsf_config_t *config_ptr = (zsf_config_t *)data_ptr;
   char *end_ptr = NULL;
 
@@ -56,6 +89,16 @@ int _zsf_ini_handler(char *section, char *key, char *value, void *data_ptr) {
       config_ptr->locks[lock_index].parameters.lock_width = strtod(value, &end_ptr);
     } else if (!strcmp(key, "lock_bottom")) {
       config_ptr->locks[lock_index].parameters.lock_bottom = strtod(value, &end_ptr);
+    } else if (!strcmp(key, "flow_profile")) {
+      int length = 0;
+      double *values = NULL;
+      length = parse_double_list(value, &values);
+      if (length) {
+        config_ptr->locks[lock_index].flow_profile.length = length;
+        config_ptr->locks[lock_index].flow_profile.values = values;
+      } else {
+        return INI_FAIL;
+      }
     }
   } else if (!strcmp(section, "general") || !*section) {
     if (!strcmp(key, "start_time")) {
@@ -80,7 +123,7 @@ int zsf_config_load(zsf_config_t *config_ptr, const char *filepath) {
   config_ptr->start_time = 0.0;
   config_ptr->end_time = 0.0;
   config_ptr->current_time = 0.0;
-  return ini_read(filepath, _zsf_ini_handler, config_ptr);
+  return ini_read(filepath, zsf_ini_handler, config_ptr);
 }
 
 void zsf_config_unload(zsf_config_t *config_ptr) {
