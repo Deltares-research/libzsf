584fe7b154b8e6d1ed294852043735b56eb64a17
Erwin Mulder <160875449+erwin-mulder@users.noreply.github.com>
2024-08-01 10:25:52 +0200

    Added asserts in test handler function. Conforming to (un)expected inputs, given the test_ini_input.ini file.

--- tests/test_ini_read.c
+++ tests/test_ini_read.c
@@ -9,7 +9,50 @@ void setUp(void) {}
 void tearDown(void) {}
 
 static int my_ini_handler(char *section, char *key, char *value, void *data_ptr) {
-  //TODO: Add some asserts here.
+  if (!strcmp(section, "section1")) {
+    if (!strcmp(key, "number")) {
+      TEST_ASSERT_EQUAL_STRING("1", value);
+    } else if (!strcmp(key, "another")) {
+      TEST_ASSERT_EQUAL_STRING("3.14", value);
+    } else if (!strcmp(key, "x_also")) {
+      TEST_ASSERT_EQUAL_STRING("5", value);
+    } else if (!strcmp(key, "y_and_then")) {
+      TEST_ASSERT_EQUAL_STRING("72", value);
+    } else if (!strcmp(key, "key")) {
+      TEST_ASSERT_EQUAL_STRING("value", value);
+    } else if (!strcmp(key, "keyname")) {
+      TEST_ASSERT_EQUAL_STRING("value with spaces", value);
+    } else if (!strcmp(key, "variable")) {
+      TEST_ASSERT_EQUAL_STRING("a value", value);
+    } else if (!strcmp(key, "var2")) {
+      TEST_ASSERT_EQUAL_STRING("value with ; included", value);
+    } else {
+      // Section start, key and value should be empty.
+      TEST_ASSERT_EQUAL_STRING("", key);
+      TEST_ASSERT_EQUAL_STRING("", value);
+    }
+  } else if (!strcmp(section, "sections can have spaces")) {
+    if (!strcmp(key, "novaluegiven")) {
+      TEST_FAIL_MESSAGE("Handler called for key with empty value.");
+    } else if (!strcmp(key, "no_value_only_comment")) {
+      TEST_ASSERT_EQUAL_STRING_MESSAGE("", value, "Comment passed as value.");
+      TEST_FAIL_MESSAGE("Handler called for key with empty value.");
+    } else if (!strcmp(key, "lots_of_spaces")) {
+      TEST_ASSERT_EQUAL_STRING("234.5", value);
+    } else if (!strcmp(key, "attribute")) {
+      TEST_ASSERT_EQUAL_STRING("42", value);
+    } else if (!strcmp(key, "#disabled")) {
+      TEST_FAIL_MESSAGE("Trying to set disabled key.");
+    } else if (!strcmp(key, ";disabled")) {
+      TEST_FAIL_MESSAGE("Trying to set disabled key.");
+    } else {
+      // Section start, key and value should be empty.
+      TEST_ASSERT_EQUAL_STRING("", key);
+      TEST_ASSERT_EQUAL_STRING("", value);
+    }
+  } else {
+    TEST_FAIL_MESSAGE("Unexpected call of handler.");
+  }
   return INI_OK;
 }
 
