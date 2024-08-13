ec4db42e5553ee485fb6ecd5f17abaac8e7681ca
Erwin Mulder <160875449+erwin-mulder@users.noreply.github.com>
2024-08-13 16:30:20 +0200

    Added retrieval of phase-wise data from timeseries and setting of parameters in sealock_update_phase_wise_parameters. Also stored addtional arguments needed for calling zsf functions in update.

--- src/load_phase_wise.h
+++ src/load_phase_wise.h
@@ -23,6 +23,17 @@ typedef struct phase_wise_row_struct {
   double density_current_factor_lake;
 } phase_wise_row_t;
 
+#  define PHASE_WISE_CLEAR_ROW()                                                                   \
+    (phase_wise_row_t){.time = 0,                                                                  \
+                       .ship_volume_lake_to_sea = 0,                                               \
+                       .ship_volume_sea_to_lake = 0,                                               \
+                       .t_flushing = 0,                                                            \
+                       .t_level = 0,                                                               \
+                       .t_open_lake = 0,                                                           \
+                       .t_open_sea = 0,                                                            \
+                       .density_current_factor_sea = 0,                                            \
+                       .density_current_factor_lake = 0};
+
 int load_phase_wise_timeseries(csv_context_t *context, char *filepath);
 
 #  if defined(__cplusplus)
