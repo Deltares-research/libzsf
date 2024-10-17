26192d043e08a65fde1481dd3c45c00bcd5f57c2
Erwin Mulder <160875449+erwin-mulder@users.noreply.github.com>
2024-10-17 12:50:23 +0200

    Do not separate flow directions if there's only one layer.

--- src/io_layer_distribution.c
+++ src/io_layer_distribution.c
@@ -152,8 +152,9 @@ int distribute_discharge_over_layers(double total_discharge, const profile_t *pr
     double relative_fraction = relative_discharge_layer / relative_discharge_total;
     printf("DEBUG ZSF: rel_tot=%g, rel_val=%g, rel_frc=%g\n", relative_discharge_total,
            relative_discharge_layer, relative_fraction);
-    if (relative_fraction <= 0)
+    if (relative_fraction <= 0 && layers->number_of_layers > 1) {
       relative_fraction = 0;
+    }
     layered_discharge_result->discharge_per_layer[layer] = relative_fraction * total_discharge;
     printf("DEBUG ZSF: layer[%d]=%g\n", layer, relative_fraction * total_discharge);
     previous_volume = next_volume;
