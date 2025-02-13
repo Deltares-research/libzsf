79c44f8933a9ac8119494a9d7e87f456b6a4b731
Erwin Mulder <160875449+erwin-mulder@users.noreply.github.com>
2025-02-13 13:54:23 +0100

    Implemented correct detection of phase durations shorter than timeseries stepping. Also improved correction of first phase where the dimr update time step is initially unknown. Also fixed one-off error when stepping to new phases.

--- src/sealock.h
+++ src/sealock.h
@@ -25,18 +25,15 @@ typedef enum zsf_computation_mode_enum {
 typedef struct zsf_phase_wise_args_struct {
   int run_update;
   int routine;
-  time_t time;      // current time
-  time_t time_step; // time step taken from previous time to current time
-  double t_level;
-  double t_open_lake;
-  double t_open_sea;
-  double t_flushing;
+  time_t time;              // current time
+  time_t time_step;         // time step taken from previous time to current time
+  time_t time_duration_end; // time when the phase ends.
+  double duration;          // duration of the active phase.
 } zsf_phase_wise_args_t;
 
-#define PHASE_WISE_CLEAR_ARGS()                                                                    \
-  (zsf_phase_wise_args_t) {                                                                        \
-    .run_update = 0, .routine = 0, .time = 0, .time_step = 0, .t_level = 0, .t_open_lake = 0,      \
-    .t_open_sea = 0, .t_flushing = 0                                                               \
+#define PHASE_WISE_CLEAR_ARGS()                                                                     \
+  (zsf_phase_wise_args_t) {                                                                         \
+    .run_update = 0, .routine = 0, .time = 0, .time_step = 0, .time_duration_end = 0, .duration = 0 \
   }
 
 #define MAX_NUM_VOLUMES 50
@@ -108,7 +105,7 @@ int sealock_init(sealock_state_t *lock, time_t start_time, unsigned int max_num_
 int sealock_load_timeseries(sealock_state_t *lock, char *filepath);
 int sealock_set_parameters_for_time(sealock_state_t *lock, time_t time);
 int sealock_update(sealock_state_t *lock, time_t time);
-int sealock_delta_time_ok(sealock_state_t *lock, time_t delta_time);
+int sealock_delta_time_ok(sealock_state_t *lock, time_t delta_time, time_t start_time);
 
 #if defined(__cplusplus)
 }
