e07b914302ed809040be377cc47d3ec426f77fc6
Erwin Mulder <160875449+erwin-mulder@users.noreply.github.com>
2025-02-13 14:57:41 +0100

    Added read support for mitigation parameters in phase-wise operation.

--- src/load_phase_wise.h
+++ src/load_phase_wise.h
@@ -21,6 +21,11 @@ typedef struct phase_wise_row_struct {
   double t_open_sea;
   double density_current_factor_sea;
   double density_current_factor_lake;
+  double distance_door_bubble_screen_lake;
+  double distance_door_bubble_screen_sea;
+  double flushing_discharge_high_tide;
+  double flushing_discharge_low_tide;
+  double sill_height_lake;
 } phase_wise_row_t;
 
 #  define PHASE_WISE_CLEAR_ROW()                                                                   \
@@ -32,7 +37,12 @@ typedef struct phase_wise_row_struct {
                        .t_open_lake = 0,                                                           \
                        .t_open_sea = 0,                                                            \
                        .density_current_factor_sea = 1.0,                                          \
-                       .density_current_factor_lake = 1.0};
+                       .density_current_factor_lake = 1.0,                                         \
+                       .distance_door_bubble_screen_lake = 0,                                      \
+                       .distance_door_bubble_screen_sea = 0,                                       \
+                       .flushing_discharge_high_tide = 0,                                          \
+                       .flushing_discharge_low_tide = 0,                                           \
+                       .sill_height_lake = 0};
 
 int load_phase_wise_timeseries(csv_context_t *context, char *filepath);
 
